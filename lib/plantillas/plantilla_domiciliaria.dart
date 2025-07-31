764169d542f71d0332b93fe9afc1527d9ebfaf9a
John Bernal <johnnever.bernal@gmail.com>
2025-07-31 14:20:43 -0500

    Commit basico

--- lib/plantillas/plantilla_domiciliaria.dart
+++ lib/plantillas/plantilla_domiciliaria.dart
@@ -155,11 +155,17 @@ class PrisionDomiciliariaTemplate {
         <div style="margin-top: 50px;">
           <img src="https://firebasestorage.googleapis.com/v0/b/tu-proceso-ya-fe845.firebasestorage.app/o/logo_tu_proceso_ya_transparente.png?alt=media&token=07f3c041-4ee3-4f3f-bdc5-00b65ac31635" width="150" height="50"><br>
         </div>
+              
+        <b>NOTA IMPORTANTE:</b><br>
+          <p style="font-size: 13px;">
+          
+          Este mensaje también será enviado a la Oficina Jurídica del establecimiento <strong>$centroPenitenciario</strong>, con el fin de dejar constancia formal de esta solicitud y facilitar el inicio oportuno de los trámites correspondientes.<br><br>
+            Esta solicitud ha sido generada mediante la plataforma tecnológica <b>Tu Proceso Ya</b>, diseñada para facilitar el ejercicio autónomo del derecho fundamental de petición por parte de las personas privadas de la libertad o sus familiares.<br><br>
 
-        <b>NOTA IMPORTANTE</b><br>
-        <p style="font-size: 13px;">
-          Este mensaje también será enviado a la Oficina Jurídica del establecimiento <strong>$centroPenitenciario</strong>, con el fin de dejar constancia formal de esta solicitud y facilitar el inicio oportuno de los trámites correspondientes.
-        </p>
+            En virtud del artículo 23 de la Constitución Política de Colombia y de las sentencias T-377 de 2014 y T-114 de 2017 de la Corte Constitucional, <b>no se requiere la firma de abogado ni apoderado para presentar una petición</b>. La plataforma actúa como medio de apoyo y canal de gestión digital, plenamente legítimo y válido.<br><br>
+
+            Exigir firma del apoderado o desconocer al solicitante por el solo hecho de que la petición fue tramitada por medio electrónico, constituye una barrera de acceso a la justicia e infringe el principio de eficacia del derecho fundamental de petición.
+          </p>
       </body>
     </html>
   """);
